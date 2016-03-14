2819f6490a0c1e6281afc4e4ba2bd8278855b670
Corielambright <Corielambright@gmail.com>
2016-03-14 09:17:52 -0400

    Update fizzbuzz_spec.js

---
+++ spec/fizzbuzz_spec.js
@@ -0,0 +1,10 @@
+var fb = require('../fizzbuzz');
+
+describe ("A fizzbuzz program", function() {
+	
+	it ("can count from 1 to 100", function() {
+		expect (fb.count(1,5).toBe("1 2 3 4 5");
+	
+	});
+
+}); 
